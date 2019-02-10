c46cd236ca791d6da6b26469833551bd97125f88
caal2096 <caal2096@gmail.com>
2019-02-10 11:25:25 -0400

    modificacion del reporte de catalogo

--- reporte/listado_catalogo.php
+++ reporte/listado_catalogo.php
@@ -22,37 +22,46 @@
    //Tamaño de la pagina letter=carta y legal=oficio, exiten más pero estos son los mas comunes 
    $lobjPdf->AddPage("P","legal");
    //SetFont(Tipo de fuente,negrilla,tamaño de la fuente)
-   $lobjPdf->SetFont("arial","B",12);
+   $lobjPdf->SetFont("arial","BU",14);
 
-   //Ln(cantidad de lineas en blanco)
-   $lobjPdf->Ln(1);
+
+   
 
    //Parametros de la función CELL (ancho,alto,texto,borde,salto de linea,alineación)
-   $lobjPdf->Cell(0,6,utf8_decode("Catalogo de los Bienes"),0,1,"C");
-   $lobjPdf->Ln(10);
+   $lobjPdf->Cell(0,6,utf8_decode("Catalogo de los Bienes Muebles"),0,1,"C");
+   $lobjPdf->ln(4);
+   $lobjPdf->SetFillColor(123,135,240);
 
-   $lobjPdf->Cell(24,6,utf8_decode("Tipo"),1,0,"C");
-   $lobjPdf->Cell(30,6,utf8_decode("Clasificacion"),1,0,"C");
-   $lobjPdf->Cell(25,6,utf8_decode("Marca"),1,0,"C");
-   $lobjPdf->Cell(25,6,utf8_decode("Modelo"),1,0,"C");
-   $lobjPdf->Cell(10,6,utf8_decode("Cant"),1,0,"C");
+
+   $lobjPdf->SetFont("arial","B",12);
+   $lobjPdf->SetX(25);
+   $lobjPdf->Cell(35,8,utf8_decode("Tipo"),0,0,"C",1);
+   $lobjPdf->Cell(40,8,utf8_decode("Clasificacion"),0,0,"C",1);
+   $lobjPdf->Cell(40,8,utf8_decode("Marca"),0,0,"C",1);
+   $lobjPdf->Cell(40,8,utf8_decode("Modelo"),0,0,"C",1);
+   $lobjPdf->Cell(20,8,utf8_decode("Cant"),0,0,"C",1);
    
    //Ln(cantidad de lineas en blanco)
-   $lobjPdf->Ln(6);
-   $lobjPdf->SetFont("arial","",9);
+   $lobjPdf->Ln(8);
+   $lobjPdf->SetFont("arial","",12);
+   $lobjPdf->SetLineWidth(0.5);
+   $lobjPdf->SetDrawColor(255,255,255,255);
+   $lobjPdf->SetFillColor(224,224,224);
    for($i=0;$i<count($listado_bien);$i++)
    {  
-      $lobjPdf->Cell(24,6,utf8_decode($listado_bien[$i]['nombretip']),1,0,"C");
-      $lobjPdf->Cell(30,6,utf8_decode($listado_bien[$i]['nombrecat']),1,0,"C");
-      $lobjPdf->Cell(25,6,utf8_decode($listado_bien[$i]['nombremar']),1,0,"C");
-      $lobjPdf->Cell(25,6,utf8_decode($listado_bien[$i]['nombremode']),1,0,"C");
-      $lobjPdf->Cell(10,6,utf8_decode($listado_bien[$i]['cantidadcat']),1,0,"C");
+      $lobjPdf->SetX(25);
+
+      $lobjPdf->Cell(35,8,utf8_decode($listado_bien[$i]['nombretip']),1,0,"C",1);
+      $lobjPdf->Cell(40,8,utf8_decode($listado_bien[$i]['nombrecat']),1,0,"C",1);
+      $lobjPdf->Cell(40,8,utf8_decode($listado_bien[$i]['nombremar']),1,0,"C",1);
+      $lobjPdf->Cell(40,8,utf8_decode($listado_bien[$i]['nombremode']),1,0,"C",1);
+      $lobjPdf->Cell(20,8,utf8_decode($listado_bien[$i]['cantidadcat']),1,0,"C",1);
 
       //Ln(cantidad de lineas en blanco)
    $lobjPdf->Ln(6);
       
    }
 
-   $lobjPdf->Output();
+   $lobjPdf->Output('I','listado de Catalogo.pdf');
 
 ?>
